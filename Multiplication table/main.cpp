90ef3374a96a23c5ddaef534cbeb1dc3dcace372
18404715032 <20141105066yh@admindeiMac-36.local>
2015-11-25 18:19:46 +0800

    原始代码
    输出9*9口诀表

--- Multiplication table/main.cpp
+++ Multiplication table/main.cpp
@@ -7,9 +7,21 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int i,j,result;
+    printf("\n");
+    for(i=1;j<10;i++)
+    {
+        for(j=1;j<10;j++)
+        {
+            result=i*j;
+            printf("%d*%d=%-3d",i,j,result);
+        }
+        printf("\n");
+    }
+    
+   
     return 0;
 }
