0ec89f37a6ad5459db07d6cfe3a24188369a8e9e
18404715032 <20141105066yh@admindeimac-36.local>
2015-11-25 18:13:00 +0800

    Initial Commit

---
+++ Multiplication table/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Multiplication table
+//
+//  Created by 杨浩 on 15/11/25.
+//  Copyright © 2015年 杨浩. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
